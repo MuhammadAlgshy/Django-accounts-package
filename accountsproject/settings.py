5d9affedf3253d52379dd08d55ca7625adc59f7d
Muhammad Algshy <muhammad.algshy@wfp.org>
2022-08-25 11:03:38 +0200

    0.5 version update settings

--- accountsproject/settings.py
+++ accountsproject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from os import environ
+import os
 from pathlib import Path
 from accounts.configurations import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-51cykb^&!ot$(8^6#q#5cizc31vpq@3!(vzmv*h25cp9i^svdg'
+SECRET_KEY = environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
